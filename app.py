416d267c68c1ca846b2c8d3f0d1663a24154e348
lexoudijk <lexoudijk@gmail.com>
2016-12-09 15:29:44 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':150, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "It costs " + str(cost[zone]) + " euros to ship to" + zone.
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
@@ -48,6 +48,7 @@ def makeWebhookResult(req):
         # "contextOut": [],
         "source": "apiai-onlinestore-shipping"
     }
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
